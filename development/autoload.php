0640c4e29faff010ae0e6bd3c23467450ced65bc
michaelwilliams <michael.williams@funsational.com>
2011-03-29 18:33:37 -0700

    Update autoload file

--- development/autoload.php
+++ development/autoload.php
@@ -1,30 +1,36 @@
 <?php
 
-require __DIR__.'/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
+    'Acme'                           => __DIR__.'/../src',
     'Application'                    => __DIR__.'/../src',
     'Bundle'                   		 => __DIR__.'/../src',
     'Funsational'                    => __DIR__.'/../src',
-    'Sonata'                         => __DIR__.'/../src',
-    'Knplabs'                        => __DIR__.'/../src',
     'FOS'                            => __DIR__.'/../src',
-    'Symfony'                        => __DIR__.'/../vendor/symfony/src',
-    'Doctrine\\Common\\DataFixtures' => __DIR__.'/../vendor/doctrine-data-fixtures/lib',
+    'Knplabs'                        => __DIR__.'/../src',
+    'Sonata'                         => __DIR__.'/../src',
+
+    'Doctrine'                       => __DIR__.'/../vendor/doctrine/lib',
     'Doctrine\\Common'               => __DIR__.'/../vendor/doctrine-common/lib',
+    'Doctrine\\Common\\DataFixtures' => __DIR__.'/../vendor/doctrine-data-fixtures/lib',
+    'Doctrine\\DBAL'                 => __DIR__.'/../vendor/doctrine-dbal/lib',
     'Doctrine\\DBAL\\Migrations'     => __DIR__.'/../vendor/doctrine-migrations/lib',
     'Doctrine\\MongoDB'              => __DIR__.'/../vendor/doctrine-mongodb/lib',
     'Doctrine\\ODM\\MongoDB'         => __DIR__.'/../vendor/doctrine-mongodb-odm/lib',
-    'Doctrine\\DBAL'                 => __DIR__.'/../vendor/doctrine-dbal/lib',
-    'Doctrine'                       => __DIR__.'/../vendor/doctrine/lib',
+
+    'JMS'                            => __DIR__.'/../vendor/bundles',
+    'Sensio'                         => __DIR__.'/../vendor/bundles',
+
+    'Assetic'                        => __DIR__.'/../vendor/assetic/src',
+    'Symfony'                        => __DIR__.'/../vendor/symfony/src',
     'Zend\\Log'                      => __DIR__.'/../vendor/zend-log',
 ));
 
 $loader->registerPrefixes(array(
-    'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
     'Twig_'            => __DIR__.'/../vendor/twig/lib',
+    'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
     'Swift_'           => __DIR__.'/../vendor/swiftmailer/lib/classes',
 ));
 $loader->register();
