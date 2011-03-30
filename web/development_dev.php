70edea5fc7bfddc4153ac6fc9ffae99475a29169
michaelwilliams <michael.williams@funsational.com>
2011-03-29 18:28:17 -0700

    Update the bootstrap files. You must run bin/build_bootstrap.php to save some common classes

--- web/development_dev.php
+++ web/development_dev.php
@@ -6,7 +6,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
     die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require_once __DIR__.'/../development/bootstrap.php';
+require_once __DIR__.'/../development/bootstrap.php.cache';
 require_once __DIR__.'/../development/DevelopmentKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
--- web/development_prod.php
+++ web/development_prod.php
@@ -1,11 +1,12 @@
 <?php
 
-require_once __DIR__.'/../development/bootstrap.php';
+require_once __DIR__.'/../development/bootstrap.php.cache';
 require_once __DIR__.'/../development/DevelopmentKernel.php';
-//require_once __DIR__.'/../development/DevelopmentCache.php';
+//require_once __DIR__.'/../development/bootstrap_cache.php.cache';
+//require_once __DIR__.'/../development/DevelopmentCache.php';;
 
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new DevelopmentKernel('prod', false);
-//$kernel = new AppCache(new DevelopmentKernel('prod', false));
+//$kernel = new DevelopmentCache(new DevelopmentKernel('prod', false));
 $kernel->handle(Request::createFromGlobals())->send();
